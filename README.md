5b1b39a1109fca8f431c211cc589a6faa9d4a001
Wendel Schulz <wendelschulz01@gmail.com>
2025-03-19 11:51:59 -0300

    Função de verificação numeros primos

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+<<<<<<< HEAD
 # desafios-scholarship
 Repositório de desafios Scholarship 
+=======
+# Desafios-scholarship
+## Cada Desafio está em sua branch respectiva
+
+* ex-js-01
+* ex-js-02
+* ex-js-03
+* ex-js-04
+
+>>>>>>> 6689634ab63cebd403ca62cb686d7b02fdf9f034
---
+++ ex-js-02/script.js
@@ -0,0 +1,24 @@
+// Importa o módulo readline
+const readline = require('readline');
+
+// Cria a interface input/output
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Função que verifica se um número é primo
+function nPrimo(numero) {
+  if (numero <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(numero); i++) {
+    if (numero % i === 0) {
+      return false; // Se divisível, não é primo
+    }
+  }
+  return true; // Se não foi divisível por nenhum número, é primo
+}
+
+// Solicita o número ao usuário
+rl.question('Digite um número para verificar se ele é primo: ', processarEntrada);
