27accfe43964c6ea3e46bd77587773261f3edd65
Wendel Schulz <wendelschulz01@gmail.com>
2025-03-19 13:12:31 -0300

    Projeto números primos

--- ex-js-02/script.js
+++ ex-js-02/script.js
@@ -20,5 +20,18 @@ function nPrimo(numero) {
   return true; // Se não foi divisível por nenhum número, é primo
 }
 
+// Função que processa a entrada do usuário
+function processarEntrada(resposta) {
+  let numero = Number(resposta);
+
+  if (ehPrimo(numero)) {
+    console.log(`${numero} é um número primo.`);
+  } else {
+    console.log(`${numero} não é um número primo.`);
+  }
+
+  rl.close(); // Encerra a interface readline
+}
+
 // Solicita o número ao usuário
 rl.question('Digite um número para verificar se ele é primo: ', processarEntrada);
